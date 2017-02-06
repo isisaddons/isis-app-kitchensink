7e9a151135dc1e61e2d844faef1d49d4bc22bcca
Johan Doornenbal <johan@filternet.nl>
2017-02-06 15:32:03 +0100

    adds mixin

---
+++ app/src/main/java/org/isisaddons/app/kitchensink/app/mixin/ReferenceObject_MoveChildren.java
@@ -0,0 +1,46 @@
+package org.isisaddons.app.kitchensink.app.mixin;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.Mixin;
+import org.apache.isis.applib.annotation.SemanticsOf;
+
+import org.isisaddons.app.kitchensink.dom.reference.ReferenceChildObject;
+import org.isisaddons.app.kitchensink.dom.reference.ReferenceChildObjects;
+import org.isisaddons.app.kitchensink.dom.reference.ReferenceObject;
+
+@Mixin
+public class ReferenceObject_MoveChildren {
+
+    private final ReferenceObject object;
+    public ReferenceObject_MoveChildren(ReferenceObject object){
+        this.object = object;
+    }
+
+    @Action(semantics = SemanticsOf.IDEMPOTENT)
+    public ReferenceObject moveChildrenMixinTest(
+            final List<ReferenceChildObject> childObjects
+            ){
+        for (ReferenceChildObject childObject : childObjects) {
+            childObject.setParent(object);
+        }
+        return object;
+    }
+
+    public List<ReferenceChildObject> choices0MoveChildrenMixinTest() {
+        return otherChildren(object);
+    }
+
+    private List<ReferenceChildObject> otherChildren(final ReferenceObject object) {
+        final List<ReferenceChildObject> referenceChildObjects = this.referenceChildObjects.listAll();
+        referenceChildObjects.removeAll(object.getChildren());
+        return referenceChildObjects;
+    }
+
+    @Inject
+    ReferenceChildObjects referenceChildObjects;
+
+}
